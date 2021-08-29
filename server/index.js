574d20e0196e63cde2b71b458691b514d594bbe9
Alex Hunter <63908354+al-mister-dog@users.noreply.github.com>
2021-08-29 21:40:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,19 +3,20 @@ const path = require('path');
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 
-const http = require('http');//
-const server = http.createServer(app);//
-const { Server } = require("socket.io");// 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io"); 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "https://arcane-castle-17364.herokuapp.com/",
     methods: ["GET", "POST"]
-  } //needs to be done!
-});// io is a new instance of Server
+  } 
+});
 
 const fileFilter = function(req, file, cb) {
   const allowedTypes = [
@@ -36,23 +37,32 @@ const fileFilter = function(req, file, cb) {
 const MAX_SIZE = 200000;
 
 const upload = multer({
-  dest: './client/src/assets',
+  dest: 'server/public/assets',
   fileFilter,
   limits: {
     fileSize: MAX_SIZE
   }
 })
  
-app.post('/upload', upload.single('file'), (req, res, next) => {
+app.post('/api/upload', upload.single('file'), (req, res, next) => {
   const old_file_path = req.file.path;
   const new_file_path = old_file_path + '.jpeg';
   fs.renameSync(old_file_path, new_file_path);
   res.send(new_file_path)
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+  
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+ }
+ 
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const randomHue = Math.floor(Math.random() * 357);
+  const background = `hsl(${randomHue}, 87%, 91%)`;
 
   io.to(socket.id).emit('send id', {
     id: socket.id
@@ -64,6 +74,7 @@ io.on('connection', (socket) => {
       message: msg.message,
       user: msg.user,
       id: socket.id,
+      background,
       date: new Date().toLocaleTimeString().slice(0,4) + new Date().toLocaleTimeString().slice(7)
     })
   });
@@ -74,6 +85,7 @@ io.on('connection', (socket) => {
       message: msg.message,
       user: msg.user,
       id: socket.id,
+      background,
       date: new Date().toLocaleTimeString().slice(0,4) + new Date().toLocaleTimeString().slice(7)
     })
   })
@@ -84,9 +96,5 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-
-
+server.listen(PORT);
 
